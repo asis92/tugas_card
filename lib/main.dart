9c7a35262035770a054b2b66785fd691a9df3ac2
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-27 22:11:29 +0700

    Step 2 Update Main Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Materi Card Week 9",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyCard(),
+    );
+  }
+}
