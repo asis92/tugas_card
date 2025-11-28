260a5284ce6fe57c59758d5fdad467dc511b8a13
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-28 18:12:07 +0700

    Step 2 Update Card Tugas Week 9

--- lib/pages/card03.dart
+++ lib/pages/card03.dart
@@ -6,11 +6,29 @@ class MyCard3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      appBar: AppBar(title: const Text("Card Sederhana"), actions: const []),
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          child: const Column(children: []),
+          child: Column(
+            children: [
+              Card(
+                elevation: 8,
+                shadowColor: Colors.green,
+                clipBehavior: Clip.antiAlias,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [Colors.blue, Colors.red],
+                    ),
+                  ),
+                  child: Text("text", style: TextStyle(fontSize: 20.0)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
