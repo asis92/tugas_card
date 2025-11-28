d659b0652aa25816d443abca934c91f325f68acb
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-28 18:45:28 +0700

    Step 5 Update Card Tugas Week 9

--- lib/pages/card03.dart
+++ lib/pages/card03.dart
@@ -26,33 +26,49 @@ class MyCard3 extends StatelessWidget {
                   ),
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Column(
                       children: [
-                        //Container dengan circle Avatar image
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(color: Colors.white, width: 4),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withValues(alpha: 0.2),
-                                blurRadius: 10,
-                                offset: Offset(0, 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            //Container dengan circle Avatar image
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  color: Colors.white,
+                                  width: 4,
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black.withValues(alpha: 0.2),
+                                    blurRadius: 10,
+                                    offset: Offset(0, 5),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                          child: CircleAvatar(
-                            radius: 60,
-                            backgroundImage: AssetImage(
-                              "assets/images/Logo.png",
+                              child: CircleAvatar(
+                                radius: 60,
+                                backgroundImage: AssetImage(
+                                  "assets/images/Logo.png",
+                                ),
+                              ),
                             ),
-                          ),
+                            SizedBox(height: 10),
+                            // Text Name
+                            Text(
+                              "Asis Dwi Saputra",
+                              style: TextStyle(
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            // Container dengan Box Decoration
+                            // Dengan text "Flutter Developer"
+                            // Row dengan 3 Row
+                          ],
                         ),
-                        // Text Name
-                        // Container dengan Box Decoration
-                        // Dengan text "Flutter Developer"
-                        // Row dengan 3 Row
                       ],
                     ),
                   ),
