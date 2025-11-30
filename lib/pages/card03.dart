fa826d47d3f69ec7508f312b109207343c048b0b
Asis Dwi Saputra <asisdwisaputra92@gmail.com>
2025-11-30 19:40:36 +0700

    Step 6 Update Card Tugas Week 9

--- lib/pages/card03.dart
+++ lib/pages/card03.dart
@@ -8,74 +8,76 @@ class MyCard3 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Card Sederhana"), actions: const []),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Card(
-                elevation: 8,
-                shadowColor: Colors.green,
-                clipBehavior: Clip.antiAlias,
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [Colors.blue, Colors.red],
-                    ),
+        child: Column(
+          children: [
+            Card(
+              elevation: 8,
+              shadowColor: Colors.red,
+              clipBehavior: Clip.antiAlias,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [Colors.blue, Colors.red],
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            //Container dengan circle Avatar image
-                            Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 4,
-                                ),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withValues(alpha: 0.2),
-                                    blurRadius: 10,
-                                    offset: Offset(0, 5),
-                                  ),
-                                ],
-                              ),
-                              child: CircleAvatar(
-                                radius: 60,
-                                backgroundImage: AssetImage(
-                                  "assets/images/Logo.png",
-                                ),
-                              ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Container dengan circle Avatar + image
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.white, width: 4),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withValues(alpha: 0.2),
+                              blurRadius: 10,
+                              offset: Offset(0, 5),
                             ),
-                            SizedBox(height: 10),
-                            // Text Name
-                            Text(
-                              "Asis Dwi Saputra",
-                              style: TextStyle(
-                                fontSize: 10.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            // Container dengan Box Decoration
-                            // Dengan text "Flutter Developer"
-                            // Row dengan 3 Row
                           ],
                         ),
-                      ],
-                    ),
+                        child: CircleAvatar(
+                          radius: 28.0,
+                          backgroundImage: AssetImage("images/Logo.png"),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Asis Dwi Saputra",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      //Text Name
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Text(
+                          "Flutter Developer",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      //Container dengan BoxDecoration Circular
+                      //dengan text "Flutter Developer"
+                      // Row dengan 3 Row
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
